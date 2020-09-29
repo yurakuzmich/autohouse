ce8de17754239d9f045fd43581cbfe760e72fb44
Yury Kuzmich <yurakuzmich@gmail.com>
2020-09-30 00:07:33 +0300

    feat: add daily working

--- code.js
+++ code.js
@@ -13,5 +13,46 @@ const modalWindow = document.querySelector('.modal-wrap');
             modalWindow.classList.toggle('modal-wrap_shown');
         }
     });
+
+
+    const allImages = document.querySelectorAll('.slider-body img');
+    let currentImage = 0;
+    allImages[0].style.width = '100%';
+
+
+    const leftButton = document.querySelector('.slider-leftarrow');
+    leftButton.addEventListener('click', () => {
+        // leftClick(currentImage, imagesCount, allImages);
+        console.log('Листаем влево');
+        if(currentImage > 0) {
+            allImages[currentImage].style.width = 0;
+            allImages[currentImage - 1].style.width = '100%';
+            currentImage -= 1;
+            console.log(currentImage);
+        } else {
+            allImages[currentImage].style.width = 0;
+            currentImage = allImages.length - 1;
+            allImages[currentImage].style.width = '100%';
+        }
+        return currentImage;
+    });
+
+    const rightButton = document.querySelector('.slider-rightarrow');
+    rightButton.addEventListener('click', () => {
+        // rightClick(currentImage);
+        console.log('Листаем вправо');
+        if(currentImage < (allImages.length - 1)) {
+            allImages[currentImage].style.width = 0;
+            allImages[currentImage + 1].style.width = '100%';
+            currentImage += 1;
+            console.log(currentImage);
+        } else {
+            allImages[currentImage].style.width = 0;
+            currentImage = 0;
+            allImages[currentImage].style.width = '100%';
+        }
+        return currentImage;        
+    });
+
 }
 
